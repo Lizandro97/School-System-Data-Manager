b9ada4e14db6c105d3b304ab784b9aba5df39d93
Lizandro97 <lizandro.aldair.7@gmail.com>
2021-11-14 21:56:04 -0500

    Agregando el index html.

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
+
+
+data = {
+    "title": "Fe y Alegria",
+    "nav_items": ["Home", "Enrollment", "Students", "Administration", "Library", "Resources"]
+}
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    global data
+    return render_template("index.html", data=data)
+
+
+@app.route('/friends')
+def friends():
+    return "My friends."
+
 
 if __name__ == '__main__':
     app.run()
